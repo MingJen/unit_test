23756e289981806ddecd63413281a20286c64652
Teemo Wang <teemo@noc.tw>
2015-09-15 08:21:26 +0800

    增加存放商品的方法
    第二個案例測不過，要先有存放商品的方法
    因為也會影響到第一個案例，所以都註解掉，只測存放商品

--- src/Cart.php
+++ src/Cart.php
@@ -1,8 +1,20 @@
 <?php
 class Cart
 {
+    private $collection;
+
+    public function __construct($product)
+    {
+        $this->collection[] = $product;
+    }
+
     public function checkout()
     {
         return 100;
+    }
+
+    public function count()
+    {
+        return count($this->collection);
     }
 }
--- tests/CartTest.php
+++ tests/CartTest.php
@@ -1,13 +1,36 @@
 <?php
 class CartTest extends PHPUnit_Framework_TestCase
 {
-    public function test第一集買了一本，其他都沒買，價格應為100X1()
+
+    public function test可以將商品放入購物車()
     {
-        $target = new Cart();
+        $stubProduct = $this->getMockBuilder('Product')
+            ->getMock();
 
-        $actual = $target->checkout();
+        $target = new Cart($stubProduct);
 
-        $this->assertEquals(100, $actual);
+        $actual = $target->count();
 
+        $this->assertEquals(1, $actual);
     }
+
+    // public function test第一集買了一本，其他都沒買，價格應為100X1()
+    // {
+    //     $target = new Cart();
+
+    //     $actual = $target->checkout();
+
+    //     $this->assertEquals(100, $actual);
+
+    // }
+
+    // public function test第一集買了一本，第二集也買了一本，95折，共195()
+    // {
+    //     $target = new Cart();
+
+    //     $actual = $target->checkout();
+
+    //     $this->assertEquals(195, $actual);
+    // }
+
 }
