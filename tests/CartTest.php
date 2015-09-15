a9ae9f125a3eb21077138f355237ee9a06f56646
Teemo Wang <teemo@noc.tw>
2015-09-15 21:49:12 +0800

    拿掉註解後，買二本書的案例還是無法通過

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -21,41 +21,41 @@ class CartTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(2, $actual);
     }
 
-    // public function test第一集買了一本，其他都沒買，價格應為100X1()
-    // {
-    //     $stubProduct = $this->getMockBuilder('Product')
-    //         ->getMock();
+    public function test第一集買了一本，其他都沒買，價格應為100X1()
+    {
+        $stubProduct = $this->getMockBuilder('Product')
+            ->getMock();
 
-    //     $stubProduct->method('getPrice')
-    //          ->willReturn(100);
-
-
-    //     $target = new Cart($stubProduct);
-
-    //     $actual = $target->checkout();
-
-    //     $this->assertEquals(100, $actual);
-
-    // }
-
-    // public function test第一集買了一本，第二集也買了一本，95折，共195()
-    // {
-    //     $stubProduct = $this->getMockBuilder('Product')
-    //         ->getMock();
-    //     $stubProduct->method('getPrice')
-    //          ->willReturn(100);
-
-    //     $stubProduct2 = $this->getMockBuilder('Product')
-    //         ->getMock();
-    //     $stubProduct2->method('getPrice')
-    //          ->willReturn(100);
+        $stubProduct->method('getPrice')
+             ->willReturn(100);
 
 
-    //     $target = new Cart([$stubProduct, $stubProduct2]);
+        $target = new Cart($stubProduct);
 
-    //     $actual = $target->checkout();
+        $actual = $target->checkout();
 
-    //     $this->assertEquals(195, $actual);
-    // }
+        $this->assertEquals(100, $actual);
+
+    }
+
+    public function test第一集買了一本，第二集也買了一本，95折，共195()
+    {
+        $stubProduct = $this->getMockBuilder('Product')
+            ->getMock();
+        $stubProduct->method('getPrice')
+             ->willReturn(100);
+
+        $stubProduct2 = $this->getMockBuilder('Product')
+            ->getMock();
+        $stubProduct2->method('getPrice')
+             ->willReturn(100);
+
+
+        $target = new Cart([$stubProduct, $stubProduct2]);
+
+        $actual = $target->checkout();
+
+        $this->assertEquals(195, $actual);
+    }
 
 }
